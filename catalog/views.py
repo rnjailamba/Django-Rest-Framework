f12c2310cbc2bb10f493f9e0e0bda211314e49d8
Rnjai Lamba <rnjailamba12@gmail.com>
2018-02-10 11:31:20 -0600

    ready to deploy

--- catalog/views.py
+++ catalog/views.py
@@ -1,10 +1,16 @@
-from django.http import HttpResponse
 from rest_framework import generics
-from rest_framework.response import Response
+from .permissions import IsAdminOrReadOnly
 from .models import Product
 from .serializers import ProductSerializer
 
 
-class ProductList(generics.ListAPIView):
+class ProductList(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    permission_classes = (IsAdminOrReadOnly, )
+
+
+class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = (IsAdminOrReadOnly, )
