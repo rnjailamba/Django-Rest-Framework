20b395d57b09c395d04432633b969d10cedcb573
Rnjai Lamba <rnjailamba12@gmail.com>
2018-02-11 22:38:41 -0600

    static files issue

--- drftutorial/settings.py
+++ drftutorial/settings.py
@@ -133,8 +133,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
